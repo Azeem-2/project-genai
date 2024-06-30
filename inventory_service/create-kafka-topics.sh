b9a9115b25bddb368c5b7f76e0c2171632488edd
Azeem000011111111 <faizanoleka@gmail.com>
2024-06-30 16:51:28 +0500

    .

--- inventory_service/create-kafka-topics.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Wait for Kafka to be ready
-while ! nc -z broker 9092; do   
-  sleep 0.1
-done
-
-# Create the 'orders' topic
-/opt/kafka/bin/kafka-topics.sh --create --topic orders --bootstrap-server broker:9092 --replication-factor 1 --partitions 1 || true
